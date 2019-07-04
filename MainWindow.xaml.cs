9d6bafb52e41ead523b425ebbf9aeef76445972f
akshayrkansara <akshayrkansara@gmail.com>
2019-07-04 18:27:31 -0400

    First Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿/* Akshay, Tyler, Shubham, Anil
+ Group No 3*/
+using System;
+using System.Windows;
+namespace TemperatureConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        decimal x;
+        decimal y;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void celToF_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                x = decimal.Parse(temp.Text);
+                y = (x * 9) / 5 + 32;
+                y = decimal.Round(y, 1);
+                lblDisp.Content = y.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("please enter only numbers to convert");
+            }
+        }
+        private void fToCel_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                x = decimal.Parse(temp.Text);
+                y = (x - 32) * 5 / 9;
+                y = decimal.Round(y, 1, System.MidpointRounding.AwayFromZero);
+                lblDisp.Content = y.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("please enter only numbers to convert");
+            }
+        }
+    }
+}
